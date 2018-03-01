5a9a5285495321323d65520ba1aa2a14ebfd260a
Renze Yu <renzeyu@microsoft.com>
2018-03-01 14:53:44 +0800

    upgrade to vs2017

--- build.cmd
+++ build.cmd
@@ -4,13 +4,10 @@ PUSHD %~dp0
 SETLOCAL
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-IF NOT DEFINED VisualStudioVersion (
-    IF DEFINED VS140COMNTOOLS (
-        CALL "%VS140COMNTOOLS%\VsDevCmd.bat"
-        GOTO :EnvSet
-    )
-
-    ECHO Error: build.cmd requires Visual Studio 2015.
+ECHO Checking Dotnet CLI version...
+dotnet --version
+IF NOT '%ErrorLevel%' == '0' (
+    ECHO Error: build.cmd requires Dotnet CLI. Please follow https://www.microsoft.com/net/core to install .NET Core."
     SET ERRORLEVEL=1
     GOTO :Exit
 )
@@ -26,56 +23,13 @@ SET CachedNuget=%LocalAppData%\NuGet\NuGet.exe
 :: nuget wrapper requires nuget.exe path in %PATH%
 SET PATH=%PATH%;%LocalAppData%\NuGet
 
-
-:: Restore packages for .csproj projects
-
-CALL :RestorePackage
-
 :: Download Doxygen
 CALL :DownloadDoxygen
 
-:: Log build command line
-SET BuildLog=%~dp0msbuild.log
-SET BuildPrefix=echo
-SET BuildPostfix=^> "%BuildLog%"
-
-CALL :Build %*
-
 :: Build
-SET BuildPrefix=
-SET BuildPostfix=
-CALL :Build %*
-IF NOT '%ErrorLevel%'=='0' (
-    GOTO :AfterBuild
-)
-
-
-:AfterBuild
-
-:: Pull the build summary from the log file
-ECHO.
-ECHO === BUILD RESULT ===
-findstr /ir /c:".*Warning(s)" /c:".*Error(s)" /c:"Time Elapsed.*" "%BuildLog%" & cd >nul
-ECHO Exit Code: %BuildErrorLevel%
-SET ERRORLEVEL=%BuildErrorLevel%
-GOTO :Exit
-
-:Build
-%BuildPrefix% msbuild "%BuildProj%" /p:Configuration=%Configuration% /nologo /maxcpucount:1 /verbosity:minimal /nodeReuse:false /fileloggerparameters:Verbosity=diag;LogFile="%BuildLog%"; %BuildPostfix%
+dotnet build %BuildProj%
 SET BuildErrorLevel=%ERRORLEVEL%
 GOTO :Exit
-
-:RestorePackage
-
-SET CachedNuget=%LocalAppData%\NuGet\NuGet.exe
-IF EXIST "%CachedNuget%" GOTO :Restore
-ECHO Downloading latest version of NuGet.exe...
-IF NOT EXIST "%LocalAppData%\NuGet" MD "%LocalAppData%\NuGet"
-powershell -NoProfile -ExecutionPolicy UnRestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CachedNuget%'"
-
-:Restore
-:: Currently has corpnet dependency
-nuget restore "%BuildProj%"
 
 :DownloadDoxygen
 SET DoxygenLocation=%~dp0src\Microsoft.Content.Build.Code2Yaml.Steps\tools
