12382c440dd63a2fd7af8164347f3d196cd8f215
Renze Yu <renzeyu@microsoft.com>
2018-03-08 15:42:17 +0800

    add test cases

---
+++ test/code2yaml.Tests/TestData/test.java
@@ -0,0 +1,13 @@
+﻿package com.mycompany.app;
+
+/**
+ * App's summary
+ */
+public class App {
+    /**
+     * Main's summary
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
